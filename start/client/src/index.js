1cc3550ff66b4568e73e601d17260d95143b1287
ramtob <ramtob@gmail.com>
2019-05-26 17:25:14 +0300

    Section 8. Manage local state

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -2,10 +2,15 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
-import { ApolloProvider } from 'react-apollo';
+import { Query, ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
 
@@ -17,6 +22,8 @@ const client = new ApolloClient({
             authorization: localStorage.getItem('token'),
         },
     }),
+    typeDefs,
+    resolvers,
 });
 
 cache.writeData({
@@ -26,7 +33,19 @@ cache.writeData({
     },
 });
 
+const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn {
+        isLoggedIn @client
+    }
+`;
+
+injectStyles();
+
 ReactDOM.render(
-<ApolloProvider client={client}>
-    <Pages />
-    </ApolloProvider>, document.getElementById('root'));
+    <ApolloProvider client={client}>
+        <Query query={IS_LOGGED_IN}>
+            {({ data }) => (data.isLoggedIn ? <Pages /> : <Login />)}
+        </Query>
+    </ApolloProvider>,
+    document.getElementById('root'),
+);
--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react';
-import { Query } from 'react-apollo';
+import React, {Fragment} from 'react';
+import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Loading from '../components/loading';
@@ -7,26 +7,27 @@ import Header from '../components/header';
 import ActionButton from '../containers/action-button';
 import LaunchDetail from '../components/launch-detail';
 
-import { LAUNCH_TILE_DATA } from './launches';
+import {LAUNCH_TILE_DATA} from './launches';
 
 export const GET_LAUNCH_DETAILS = gql`
-  query LaunchDetails($launchId: ID!) {
-    launch(id: $launchId) {
-      site
-      rocket {
-        type
-      }
-      ...LaunchTile
+    query LaunchDetails($launchId: ID!) {
+        launch(id: $launchId) {
+            isInCart @client
+            site
+            rocket {
+                type
+            }
+            ...LaunchTile
+        }
     }
-  }
-  ${LAUNCH_TILE_DATA}
+    ${LAUNCH_TILE_DATA}
 `;
 
-export default function Launch({ launchId }) {
+export default function Launch({launchId}) {
     return (
-        <Query query={GET_LAUNCH_DETAILS} variables={{ launchId }}>
-            {({ data, loading, error }) => {
-                if (loading) return <Loading />;
+        <Query query={GET_LAUNCH_DETAILS} variables={{launchId}}>
+            {({data, loading, error}) => {
+                if (loading) return <Loading/>;
                 if (error) return <p>ERROR: {error.message}</p>;
 
                 return (
